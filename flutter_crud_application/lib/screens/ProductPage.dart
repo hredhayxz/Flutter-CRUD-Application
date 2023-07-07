2446c23326a4d5ac4f6d1b0d85cbfee0eb075ea0
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-07-07 21:59:02 +0600

    Fetch Data from Database is working (Under developing - Not complete)
    Fetch Data from Database is working (Under developing - Not complete)

--- flutter_crud_application/lib/screens/ProductPage.dart
+++ flutter_crud_application/lib/screens/ProductPage.dart
@@ -30,80 +30,93 @@ class _ProductPageState extends State<ProductPage> {
       appBar: AppBar(
         title: const Text('Product List'),
         centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.refresh,
+            ),
+            onPressed: () {
+              fetchData();
+              if (mounted) {
+                setState(() {});
+              }
+            },
+          )
+        ],
       ),
       body: inProgress
           ? const Center(
-              child: CircularProgressIndicator(),
-            )
+        child: CircularProgressIndicator(),
+      )
           : ListView.separated(
-              itemCount: productList.length,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  onLongPress: () {
-                    showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            titlePadding: const EdgeInsets.only(left: 16),
-                            contentPadding: const EdgeInsets.only(
-                                left: 8, right: 8, bottom: 8),
-                            title: Row(
-                              children: [
-                                const Text('Choose an action'),
-                                const Spacer(),
-                                IconButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                  icon: const Icon(Icons.close),
-                                )
-                              ],
-                            ),
-                            content: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                ListTile(
-                                  onTap: () {},
-                                  leading: const Icon(Icons.edit),
-                                  title: const Text("Tap here for edit"),
-                                ),
-                                const SizedBox(
-                                  height: 5,
-                                ),
-                                ListTile(
-                                  onTap: () {},
-                                  leading: const Icon(Icons.delete),
-                                  title: const Text("Tap here for delete"),
-                                ),
-                              ],
-                            ),
-                          );
-                        });
-                  },
-                  leading: Image.network(
-                    productList[index].img,
-                    width: 50,
-                    errorBuilder: (_, __, ___) {
-                      return const Icon(Icons.image);
-                    },
-                  ),
-                  title: Text(productList[index].productName),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Product Code: ${productList[index].productCode}'),
-                      Text('Unit Price: ${productList[index].unitPrice}'),
-                      Text('Available Units: ${productList[index].qty}'),
-                    ],
-                  ),
-                  trailing:
-                      Text('Total Price: ${productList[index].totalPrice}'),
-                );
-              },
-              separatorBuilder: (BuildContext context, int index) {
-                return const Divider();
+        itemCount: productList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            onLongPress: () {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      titlePadding: const EdgeInsets.only(left: 16),
+                      contentPadding: const EdgeInsets.only(
+                          left: 8, right: 8, bottom: 8),
+                      title: Row(
+                        children: [
+                          const Text('Choose an action'),
+                          const Spacer(),
+                          IconButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            icon: const Icon(Icons.close),
+                          )
+                        ],
+                      ),
+                      content: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          ListTile(
+                            onTap: () {},
+                            leading: const Icon(Icons.edit),
+                            title: const Text("Tap here for edit"),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          ListTile(
+                            onTap: () {},
+                            leading: const Icon(Icons.delete),
+                            title: const Text("Tap here for delete"),
+                          ),
+                        ],
+                      ),
+                    );
+                  });
+            },
+            leading: Image.network(
+              productList[index].img,
+              width: 50,
+              errorBuilder: (_, __, ___) {
+                return const Icon(Icons.image);
               },
             ),
+            title: Text(productList[index].productName),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Product Code: ${productList[index].productCode}'),
+                Text('Unit Price: ${productList[index].unitPrice}'),
+                Text('Available Units: ${productList[index].qty}'),
+              ],
+            ),
+            trailing:
+            Text('Total Price: ${productList[index].totalPrice}'),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return const Divider();
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
@@ -119,7 +132,7 @@ class _ProductPageState extends State<ProductPage> {
   Future<void> fetchData() async {
     inProgress = true;
     final Response response =
-        await get(Uri.parse('https://crud.teamrabbil.com/api/v1/ReadProduct'));
+    await get(Uri.parse('https://crud.teamrabbil.com/api/v1/ReadProduct'));
     final Map<String, dynamic> decodedResponse = jsonDecode(response.body);
     if (response.statusCode == 200 && decodedResponse['status'] == 'success') {
       for (var it in decodedResponse['data']) {
@@ -130,9 +143,9 @@ class _ProductPageState extends State<ProductPage> {
     if (mounted) {
       setState(() {});
     }
-    print(productList.length);
-    print(decodedResponse['data'].length);
-    print(response.statusCode);
-    print(response.body);
+    // print(productList.length);
+    // print(decodedResponse['data'].length);
+    // print(response.statusCode);
+    // print(response.body);
   }
 }
